863572969008df0b1890a741b0165f8bfb965799
lucaszen123 <lsentinger@gmail.com>
2025-08-21 17:52:05 -0300

    Melhorias módulo 14 - EBAC

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -26,24 +26,20 @@ describe('Testes da Funcionalidade Usuários', () => {
     })
   })
 
-  it('Deve cadastrar um usuário com sucesso', () => {
-    cy.request('POST', '/usuarios', {
-      nome: 'Usuário Teste',
-      email: `teste${Date.now()}@qa.com`,
-      password: '123456',
-      administrador: 'true'
-    }).then((response) => {
-      expect(response.status).to.eq(201)
-      expect(response.body.message).to.eq('Cadastro realizado com sucesso')
-      expect(response.body).to.have.property('_id')
-    })
+ it('Deve cadastrar um usuário com sucesso', () => {
+  cy.criarUsuario({
+    nome: 'Usuário Teste',
+    email: `teste${Date.now()}@qa.com`,
+    password: '123456',
+    administrador: 'true'
   })
+})
 
   it('Deve validar um usuário com email inválido', () => {
     cy.request({
       method: 'POST',
       url: '/usuarios',
-      failOnStatusCode: false, // para capturar o erro sem quebrar
+      failOnStatusCode: false, 
       body: {
         nome: 'Usuário Inválido',
         email: 'email_invalido',
@@ -58,40 +54,30 @@ describe('Testes da Funcionalidade Usuários', () => {
   })
 
   it('Deve editar um usuário previamente cadastrado', () => {
-    cy.request('POST', '/usuarios', {
-      nome: 'Usuário Editar',
-      email: `editar${Date.now()}@qa.com`,
-      password: '123456',
-      administrador: 'true'
-    }).then((res) => {
-      const userId = res.body._id
-
-      cy.request('PUT', `/usuarios/${userId}`, {
-        nome: 'Usuário Editado',
-        email: `editado${Date.now()}@qa.com`,
-        password: '654321',
-        administrador: 'false'
-      }).then((respPut) => {
-        expect(respPut.status).to.eq(200)
-        expect(respPut.body.message).to.eq('Registro alterado com sucesso')
-      })
+  cy.criarUsuario({
+    nome: 'Usuário Editar',
+    email: `editar${Date.now()}@qa.com`,
+    password: '123456',
+    administrador: 'true'
+  }).then((usuario) => {
+    cy.editarUsuario(usuario._id, {
+      nome: 'Usuário Editado',
+      email: `editado${Date.now()}@qa.com`,
+      password: '654321',
+      administrador: 'false'
     })
   })
+})
 
-  it('Deve deletar um usuário previamente cadastrado', () => {
-    cy.request('POST', '/usuarios', {
-      nome: 'Usuário Deletar',
-      email: `deletar${Date.now()}@qa.com`,
-      password: '123456',
-      administrador: 'true'
-    }).then((res) => {
-      const userId = res.body._id
-
-      cy.request('DELETE', `/usuarios/${userId}`).then((respDel) => {
-        expect(respDel.status).to.eq(200)
-        expect(respDel.body.message).to.eq('Registro excluído com sucesso')
-      })
-    })
+ it('Deve deletar um usuário previamente cadastrado', () => {
+  cy.criarUsuario({
+    nome: 'Usuário Deletar',
+    email: `deletar${Date.now()}@qa.com`,
+    password: '123456',
+    administrador: 'true'
+  }).then((usuario) => {
+    cy.deletarUsuario(usuario._id)
   })
+})
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,23 @@
-Cypress.Commands.add('token', (email, senha) => {
-    cy.request({
-        method: 'POST',
-        url: 'login',
-        body: {
-            "email": email,
-            "password": senha 
-        }
-    }).then((response) => {
-        expect(response.status).to.equal(200)
-        return response.body.authorization
-    })
- })
+Cypress.Commands.add('criarUsuario', (usuario) => {
+  return cy.request('POST', '/usuarios', usuario).then((response) => {
+    expect(response.status).to.eq(201)
+    expect(response.body.message).to.eq('Cadastro realizado com sucesso')
+    return response.body // retorna os dados do usuário criado
+  })
+})
 
- Cypress.Commands.add('cadastrarProduto' , (token, produto, preco, descricao, quantidade) =>{
-    cy.request({
-        method: 'POST', 
-        url: 'produtos',
-        headers: {authorization: token}, 
-        body: {
-            "nome": produto,
-            "preco": preco,
-            "descricao": descricao,
-            "quantidade": quantidade
-          }, 
-          failOnStatusCode: false
-    })
- })
+Cypress.Commands.add('editarUsuario', (userId, usuario) => {
+  return cy.request('PUT', `/usuarios/${userId}`, usuario).then((response) => {
+    expect(response.status).to.eq(200)
+    expect(response.body.message).to.eq('Registro alterado com sucesso')
+    return response.body
+  })
+})
+
+Cypress.Commands.add('deletarUsuario', (userId) => {
+  return cy.request('DELETE', `/usuarios/${userId}`).then((response) => {
+    expect(response.status).to.eq(200)
+    expect(response.body.message).to.eq('Registro excluído com sucesso')
+    return response.body
+  })
+})
